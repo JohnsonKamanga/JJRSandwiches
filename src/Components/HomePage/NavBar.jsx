9aecdb7b90a91633528a275f8c3c24e6f62d3e9c
JohnsonKamanga <johnsonkamanga2@gmail.com>
2024-06-16 19:07:57 +0200

    updated tabs and routing links

--- src/Components/HomePage/NavBar.jsx
+++ src/Components/HomePage/NavBar.jsx
@@ -7,7 +7,7 @@ export default function NavBar() {
   const [dropDownIsOpen, setDropDownIsOpen] = useState(false);
 
   return (
-    <div className="flex flex-row h-[7%] sm:h-[8%] md:h-[6%] lg:h-[5%]">
+    <div className="flex flex-row h-[7%] sm:h-[8%] md:h-[6%] lg:h-[5%] xl:h-[8%]">
       <div className="w-[20%] sm:w-[15%] p-1 sm:p-2 text-white bg-black">
         logo
       </div>
@@ -36,33 +36,33 @@ export default function NavBar() {
 
         <div className=" hover:text-amber-400 mx-[2%] sm:mx-[3%] flex items-center hover:cursor-pointer">
           <NavLink
-            to="/Ratings"
+            to="/Communities"
             className={({ isActive }) =>
               isActive ? "border-b-[1px] border-black" : "text-inherit"
             }
           >
-            <span>Ratings</span>
+            <span>Communities</span>
           </NavLink>
         </div>
 
         <div className="hover:text-amber-400 mx-[2%] sm:mx-[3%] hover:cursor-pointer flex items-center">
           <NavLink
-            to="/Feedback"
+            to="/AboutUs"
             className={({ isActive }) =>
               isActive ? "border-b-[1px] border-black" : "text-inherit"
             }
           >
-            <span>Feedback</span>
+            <span>About Us</span>
           </NavLink>
         </div>
         <div className="hover:text-amber-400 mx-[2%] sm:mx-[3%] hover:cursor-pointer flex flex-col justify-center">
           <NavLink
-            to="/Post"
+            to="/FAQ"
             className={({ isActive }) =>
               isActive ? "border-b-[1px] border-black" : "text-inherit"
             }
           >
-            <span>Post</span>
+            <span>FAQ</span>
           </NavLink>
         </div>
       </div>
@@ -72,16 +72,20 @@ export default function NavBar() {
           className="w-[15px] sm:w-[20px] md:w-[30px] hover:w-[20px] sm:hover:w-[25px] md:hover:w-[35px] h-[15px] sm:h-[20px] md:h-[30px] hover:h-[20px] sm:hover:h-[25px] md:hover:h-[35px] mr-[15%]"
           onClick={() => {
             setDropDownIsOpen(!dropDownIsOpen);
-
-            console.log(`dropDownIsOpen is ${dropDownIsOpen}`);
           }}
         />
         {dropDownIsOpen && (
-          <div className="flex flex-col p-3 text-xs sm:text-sm md:text-base md:p-4 text-center items-center rounded-md border-[1px] border-black bg-white border-opacity-60 absolute top-[90%] -left-[170%] sm:-left-[80%] md:-left-[65%] lg:-left-[40%] xl:-left-[9%] z-40">
-            <div className="hover:cursor-pointer hover:font-[500]">
-              Manage Account
-            </div>
-            <div className="hover:cursor-pointer hover:font-[500]">Upload New Recipe</div>
+          <div className="flex flex-col p-3 text-xs sm:text-sm md:text-base md:p-4 text-center items-center rounded-md border-[1px] border-black bg-white border-opacity-60 absolute top-[90%] -left-[140%] sm:-left-[150%] md:-left-[150%] lg:-left-[100%] xl:-left-[55%] z-40">
+            <NavLink to="/AccountPage">
+              <div className="hover:cursor-pointer hover:font-[500]">
+                Manage Account
+              </div>
+            </NavLink>
+            <NavLink to="/Post">
+              <div className="hover:cursor-pointer hover:font-[500]">
+                Upload New Recipe
+              </div>
+            </NavLink>
           </div>
         )}
       </div>
