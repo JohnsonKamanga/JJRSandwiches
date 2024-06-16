76dce099416082056b2a5fd497a348f459852218
JohnsonKamanga <johnsonkamanga2@gmail.com>
2024-06-16 19:05:39 +0200

    added a FAQ page

---
+++ src/Components/FAQ/FAQ.jsx
@@ -0,0 +1,12 @@
+import NavBar from "../HomePage/NavBar";
+import Footer from "../HomePage/Footer";
+
+export default function FAQ() {
+  return (
+    <div className="flex flex-col min-h-full h-screen min-w-full w-fit">
+      <NavBar />
+      <div className="h-[88%]">FAQ page</div>
+      <Footer />
+    </div>
+  );
+}
