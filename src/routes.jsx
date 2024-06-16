300df6519f3ae67b1bb7fe57e27579990bb3d510
JohnsonKamanga <johnsonkamanga2@gmail.com>
2024-06-16 19:04:53 +0200

    updated system routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,43 +1,53 @@
-import {createBrowserRouter} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./Components/HomePage/HomePage";
 import Recipes from "./Components/Recipes/Recipes";
-import Ratings from "./Components/Ratings/Ratings";
-import Feeback from "./Components/Feedback/Feedback";
-import LoginPage from "./Components/Authentication/LoginPage";
+import AboutUs from "./Components/AboutUs/AboutUs";
+import FAQ from "./Components/FAQ/FAQ";
+import LoginPage from "./Components/Accounts/LoginPage";
 import Post from "./Components/Post/Post";
 import RecipeInstructions from "./Components/Recipes/RecipeInstructions";
+import Communities from "./Components/Communities/Communities";
+import AccountPage from "./Components/Accounts/AccountPage";
 
 export const router = createBrowserRouter([
-    {
-        path: "/" ,
-        element: <HomePage/>
-    },
-    {
-        path: "/HomePage" ,
-        element: <HomePage/>
-    },
-    {
-        path: "/Recipes",
-        element: <Recipes/>
-    },
-    {
-        path: "/Ratings",
-        element: <Ratings/>
-    },
-    {
-        path: "/Feedback",
-        element: <Feeback/>
-    },
-    {
-        path: "/LoginPage",
-        element: <LoginPage/>
-    },
-    {
-        path: "/Post",
-        element: <Post/>
-    },
-    {
-        path: "/RecipeInstructions",
-        element: <RecipeInstructions/>
-    }
-])
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/HomePage",
+    element: <HomePage />,
+  },
+  {
+    path: "/Recipes",
+    element: <Recipes />,
+  },
+  {
+    path: "/FAQ",
+    element: <FAQ />,
+  },
+  {
+    path: "/LoginPage",
+    element: <LoginPage />,
+  },
+  {
+    path: "/Post",
+    element: <Post />,
+  },
+  {
+    path: "/RecipeInstructions",
+    element: <RecipeInstructions />,
+  },
+  {
+    path: "/Communities",
+    element: <Communities />,
+  },
+  {
+    path: "/AboutUs",
+    element: <AboutUs />,
+  },
+  {
+    path: "/AccountPage",
+    element: <AccountPage />,
+  },
+]);
