f21ed57ce56c7864e6502eed61f08ff3fbffc1c4
JohnsonKamanga <johnsonkamanga2@gmail.com>
2024-06-16 19:05:06 +0200

    added about us page

---
+++ src/Components/AboutUs/AboutUs.jsx
@@ -0,0 +1,12 @@
+import NavBar from "../HomePage/NavBar";
+import Footer from "../HomePage/Footer";
+
+export default function AboutUs() {
+  return (
+    <div className="flex flex-col min-h-full h-screen">
+      <NavBar />
+      <div className="h-[86%]">AboutUs contents</div>
+      <Footer />
+    </div>
+  );
+}
