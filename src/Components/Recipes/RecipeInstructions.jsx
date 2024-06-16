5091152f0e8b0214c16a638bc330af53fdf71af4
JohnsonKamanga <johnsonkamanga2@gmail.com>
2024-06-16 19:06:44 +0200

    added a recipe instructions page

---
+++ src/Components/Recipes/RecipeInstructions.jsx
@@ -0,0 +1,12 @@
+import NavBar from "../HomePage/NavBar";
+import Footer from "../HomePage/Footer";
+
+export default function RecipeInstructions() {
+  return (
+    <div className="min-h-full h-screen">
+      <NavBar />
+      <div className="h-[88%]">Recipe Instructions</div>
+      <Footer />
+    </div>
+  );
+}
