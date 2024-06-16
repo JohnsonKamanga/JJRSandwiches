2d2fb78129c6d967394f1ad0e4060b8fda817526
JohnsonKamanga <johnsonkamanga2@gmail.com>
2024-06-16 19:08:14 +0200

    added a recipe posting page

---
+++ src/Components/Post/Post.jsx
@@ -0,0 +1,12 @@
+import Footer from "../HomePage/Footer";
+import NavBar from "../HomePage/NavBar";
+
+export default function Post() {
+  return (
+    <div className="min-h-full h-screen">
+      <NavBar />
+      <div className="h-[88%]">posting content</div>
+      <Footer />
+    </div>
+  );
+}
