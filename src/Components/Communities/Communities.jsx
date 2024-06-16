5edda452c6321a4c5924af6ce2c9cdd3d3c5ac5b
JohnsonKamanga <johnsonkamanga2@gmail.com>
2024-06-16 19:06:06 +0200

    added a communities page

---
+++ src/Components/Communities/Communities.jsx
@@ -0,0 +1,12 @@
+import NavBar from "../HomePage/NavBar";
+import Footer from "../HomePage/Footer";
+
+export default function Communities() {
+  return (
+    <div className="flex flex-col min-h-full h-screen">
+      <NavBar />
+      <div className="h-[88%]">Communties</div>
+      <Footer />
+    </div>
+  );
+}
