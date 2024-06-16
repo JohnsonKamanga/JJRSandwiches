3264fe5d5f92e3668326d8e2f95f420b0dbfeb79
JohnsonKamanga <johnsonkamanga2@gmail.com>
2024-06-16 18:16:26 +0200

    modified responsiveness on homepage main content

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -2,13 +2,12 @@ import NavBar from "./NavBar";
 import Main from "./Main";
 import Footer from "./Footer";
 
-export default function HomePage(){
-
-    return (
-        <div className="flex flex-col min-h-full h-screen ">
-            <NavBar/>
-            <Main/>
-            <Footer/>
-        </div>
-    )
+export default function HomePage() {
+  return (
+    <div className="flex flex-col min-h-full h-screen min-w-full w-fit">
+      <NavBar />
+      <Main />
+      <Footer />
+    </div>
+  );
 }
