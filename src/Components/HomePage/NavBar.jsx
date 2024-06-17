df26c397dbf0ac2cc9f7d2a3a7bcdc432a3564e0
JohnsonKamanga <johnsonkamanga2@gmail.com>
2024-06-18 00:00:12 +0200

    added gradient background and logo

--- src/Components/HomePage/NavBar.jsx
+++ src/Components/HomePage/NavBar.jsx
@@ -2,17 +2,16 @@ import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
+import Logo from "../../../public/logo-no-background.png";
 
 export default function NavBar() {
   const [dropDownIsOpen, setDropDownIsOpen] = useState(false);
 
   return (
-    <div className="flex flex-row h-[7%] sm:h-[8%] md:h-[6%] lg:h-[5%] xl:h-[8%]">
-      <div className="w-[20%] sm:w-[15%] p-1 sm:p-2 text-white bg-black">
-        logo
-      </div>
-      <div className="flex flex-row w-[70%] sm:w-[75%] font-medium text-[15px]  text-xs sm:text-sm md:text-base p-1 sm:p-2">
-        <div className="hover:text-amber-400 mx-[1.5%] sm:mx-[3%] flex items-center hover:cursor-pointer">
+    <div className="flex flex-row popup h-[7%] sm:h-[8%] md:h-[8%] lg:h-[8%] xl:h-[8%]">
+        <img src={Logo} className="bg-white border-b-[1px] border-gray-300 p-[9px] aspect-[2.5/1] sm:aspect-[3/1] lg:aspect-auto"/>
+      <div className="flex flex-row w-[70%] sm:w-[75%] font-medium text-center text-[10px] sm:text-sm md:text-base p-1 sm:p-2">
+        <div className="hover:text-[#f29260] mx-[1.5%] sm:mx-[3%] flex items-center hover:cursor-pointer">
           <NavLink
             to="/HomePage"
             className={({ isActive }) =>
@@ -23,7 +22,7 @@ export default function NavBar() {
           </NavLink>
         </div>
 
-        <div className=" hover:text-amber-400 mx-[2%] sm:mx-[3%] flex items-center hover:cursor-pointer">
+        <div className=" hover:text-[#f29260] mx-[2%] sm:mx-[3%] flex items-center hover:cursor-pointer">
           <NavLink
             to="/Recipes"
             className={({ isActive }) =>
@@ -34,7 +33,7 @@ export default function NavBar() {
           </NavLink>
         </div>
 
-        <div className=" hover:text-amber-400 mx-[2%] sm:mx-[3%] flex items-center hover:cursor-pointer">
+        <div className=" hover:text-[#f29260] mx-[2%] sm:mx-[3%] flex items-center hover:cursor-pointer">
           <NavLink
             to="/Communities"
             className={({ isActive }) =>
@@ -45,7 +44,7 @@ export default function NavBar() {
           </NavLink>
         </div>
 
-        <div className="hover:text-amber-400 mx-[2%] sm:mx-[3%] hover:cursor-pointer flex items-center">
+        <div className="hover:text-[#f29260] mx-[2%] sm:mx-[3%] hover:cursor-pointer flex items-center">
           <NavLink
             to="/AboutUs"
             className={({ isActive }) =>
@@ -55,7 +54,7 @@ export default function NavBar() {
             <span>About Us</span>
           </NavLink>
         </div>
-        <div className="hover:text-amber-400 mx-[2%] sm:mx-[3%] hover:cursor-pointer flex flex-col justify-center">
+        <div className="hover:text-[#f29260] mx-[2%] sm:mx-[3%] hover:cursor-pointer flex flex-col justify-center">
           <NavLink
             to="/FAQ"
             className={({ isActive }) =>
@@ -75,7 +74,7 @@ export default function NavBar() {
           }}
         />
         {dropDownIsOpen && (
-          <div className="flex flex-col p-3 text-xs sm:text-sm md:text-base md:p-4 text-center items-center rounded-md border-[1px] border-black bg-white border-opacity-60 absolute top-[90%] -left-[140%] sm:-left-[150%] md:-left-[150%] lg:-left-[100%] xl:-left-[55%] z-40">
+          <div className="flex flex-col w-[200%] lg:w-[129%] p-3 text-xs sm:text-sm md:text-base md:p-4 text-center items-center rounded-md border-[1px] border-black popup border-opacity-60 absolute top-[90%] -left-[140%] sm:-left-[150%] md:-left-[150%] lg:-left-[100%] xl:-left-[55%] z-40">
             <NavLink to="/AccountPage">
               <div className="hover:cursor-pointer hover:font-[500]">
                 Manage Account
