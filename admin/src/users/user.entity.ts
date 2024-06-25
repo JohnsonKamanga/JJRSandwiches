ed76b3e68962ec2e4d7f68ada0677c4c62a069aa
JohnsonKamanga <johnsonkamanga2@gmail.com>
2024-06-25 21:59:45 +0200

    updated the user entity attributes

--- admin/src/users/user.entity.ts
+++ admin/src/users/user.entity.ts
@@ -6,10 +6,31 @@ export class User{
     id: number;
 
     @Column({default : ''})
+    username: string;
+
+    @Column({default : ''})
+    userEmail: string;
+
+    @Column({default : ''})
     firstName: string;
 
     @Column({default : ''})
     lastName: string;
+
+    @Column({default : ''})
+    password: string;
+
+    @Column({default : ''})
+    location: string;
+
+    @Column({default : ''})
+    bio: string;
+
+    @Column({default : '9999-01-01'})
+    dob: string;
+
+    @Column({default : ''})
+    profilePicture: string;
 
     @Column({default : false})
     isActive: boolean;
