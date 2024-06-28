b4aa28e0b9449704c41f94c4b886309806600b95
JohnsonKamanga <johnsonkamanga2@gmail.com>
2024-06-28 15:26:49 +0200

    updated the ingredients controller and services

--- admin/src/ingredient/ingredient.service.ts
+++ admin/src/ingredient/ingredient.service.ts
@@ -1,13 +1,39 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from "typeorm";
+import { Repository } from 'typeorm';
 import { Ingredient } from './ingredient.entity';
 
 @Injectable()
-export class IngredientService {
+export class IngredientsService {
+  constructor(
+    @InjectRepository(Ingredient)
+    private ingredientRepository: Repository<Ingredient>,
+  ) {}
 
-    constructor(
-        @InjectRepository(Ingredient)
-        private ingredientRepository: Repository<Ingredient>,
-    ){}
+  async findAll(): Promise<Ingredient[]> {
+    return await this.ingredientRepository.find();
+  }
+
+  async findOneById(id): Promise<Ingredient | null> {
+    return await this.ingredientRepository.findOneBy({ id });
+  }
+
+  async findOneByRecipe(recipe): Promise<Ingredient[] | null> {
+    return await this.ingredientRepository.find({
+      where: {
+        recipe: recipe,
+      },
+      relations: {
+        recipe: true,
+      },
+    });
+  }
+
+  async create(body): Promise<any> {
+    return await this.ingredientRepository.save(body);
+  }
+
+  async delete(id): Promise<any> {
+    return await this.ingredientRepository.delete(id);
+  }
 }
