9d53068a20147bc2e42575cbf6954485f468b097
JohnsonKamanga <johnsonkamanga2@gmail.com>
2024-07-04 00:44:00 +0200

    configured user sessions

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,36 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes.jsx";
 import { UserContext } from "./Components/Accounts/UserContext.jsx";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import useToken from "./Components/Accounts/Tokens.js";
 
 function App() {
-
+  const baseurl = "http://localhost:8000/api";
   const [currentUserData, setCurrentUserData] = useState({});
-  const [currentUserToken, setCurrentUserToken] = useState("");
-  const [isSignedIn, setIsSignedIn] = useState(false);
+  const {token, setToken} = useToken();
+  const [isSignedIn, setIsSignedIn] = useState(token ? true: false);
+  const [userName, setUserName] = useState("");
+  const [userID, setUserID] = useState(0);
 
   return (
-    <UserContext.Provider value={{currentUserData, setCurrentUserData, currentUserToken, setCurrentUserToken, isSignedIn, setIsSignedIn}} >
-    <RouterProvider router={router}>
-
-    </RouterProvider>
+    <UserContext.Provider
+      value={{
+        currentUserData,
+        setCurrentUserData,
+        token,
+        userID,
+        userName,
+        setUserName,
+        setUserID,
+        setToken,
+        isSignedIn,
+        setIsSignedIn,
+      }}
+    >
+      <RouterProvider router={router}></RouterProvider>
     </UserContext.Provider>
-  )
+  );
 }
 
-export default App
+export default App;
