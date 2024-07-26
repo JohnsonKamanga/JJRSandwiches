eb26ab231ac770a9452548917ae7a8e0e214804f
JohnsonKamanga <johnsonkamanga2@gmail.com>
2024-07-26 23:12:28 +0200

    improved responsiveness of the page by adding a function that will dynamically adjust the height of the page based on the height of the recipe and instructions table

--- FrontEnd/src/Components/Recipes/RecipeInstructions.jsx
+++ FrontEnd/src/Components/Recipes/RecipeInstructions.jsx
@@ -5,119 +5,115 @@ import { Ingredient } from "./Ingredients";
 import Image1 from "./image4.jpg";
 import BgImage from "./image5.jpg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowLeftLong, faUserCircle } from "@fortawesome/free-solid-svg-icons";
+import {
+  faArrowLeftLong,
+  faUserCircle,
+} from "@fortawesome/free-solid-svg-icons";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { useLoaderData } from "react-router-dom";
 import { baseurl } from "../../routes";
 
-const ingredients = [
-  new Ingredient("Genoa salami", "3 slices"),
-  new Ingredient("mortadella", "3 slices"),
-  new Ingredient("capicola", "3 slices"),
-  new Ingredient("provolone cheese", "3 slices"),
-  new Ingredient("shredded lettuce", "1/4"),
-  new Ingredient("thinly sliced medium tomato", "1"),
-  new Ingredient("thinly sliced red onion", "1/4"),
-  new Ingredient("olive oil", "2 tablespoons"),
-  new Ingredient("red wine vinega", "1 tablespoon"),
-  new Ingredient("dried oregano", "1 tablespoon"),
-  new Ingredient("salt", "to taste"),
-  new Ingredient("pepper", "to taste"),
-  new Ingredient("mayonnaise", "1 tablespoon"),
-  new Ingredient("yellow mustard", "1 tablespoon"),
-];
-
-const instructions = [
-  "Slice the Italian sub roll horizontally, but do not cut all the way through. Open it up like a book.",
-  "Start by layering the slices of Genoa salami, followed by the mortadella, capicola, and then the provolone cheese on one side of the roll.",
-  "Evenly distribute the shredded lettuce over the meats and cheese.",
-  "Add the thin slices of tomato and red onion.",
-  "If using, add the banana peppers.",
-  "Drizzle the olive oil and red wine vinegar evenly over the vegetables.",
-  "Sprinkle the dried oregano, salt, and pepper to taste.",
-  "If desired, spread mayonnaise on the top half of the roll and mustard on the bottom half before closing the sandwich.",
-  "Close the sandwich and press down gently to help the ingredients meld together.",
-  "Cut the sandwich into halves or thirds for easier handling.",
-  "Serve immediately and enjoy your classic Italian sub sandwich!",
-];
-
-/*const recipe = new Recipe(
-  "Classic Italian Sub Sandwich",
-  Image1,
-  ingredients,
-  "15 minutes",
-  instructions
-);*/
-
 export default function RecipeInstructions() {
   const recipe = useLoaderData();
+  const [image, setImage] = useState();
+  const drawIngredients = (ingredient) => {
+    return (
+      <div
+        key={ingredient?.id}
+        className="p-2 flex flex-row text-start text-sm md:text-lg"
+      >
+        <div className="font-[400] mr-1 text-end w-[40%]">
+          {ingredient?.name}: {ingredient?.quantity}
+        </div>
+      </div>
+    );
+  };
+
+  const drawInstructions = (instruction) => {
+    return (
+      <div
+        key={instruction.instruction}
+        className="p-2 text-sm md:text-lg text-start"
+      >
+        <span className="font-medium">
+          {recipe.instructions.indexOf(instruction) + 1}
+        </span>
+        . {instruction.instruction}
+      </div>
+    );
+  };
+
+  useEffect(() => {}, []);
+
+  useEffect(() => {
+    axios
+      .get(`${baseurl}/recipes/recipe-pictures/${recipe.id}`, {
+        responseType: "blob",
+      })
+      .then((img) => {
+        setImage(URL.createObjectURL(img.data));
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
     <div className="min-h-full h-screen">
       <NavBar />
       <div
-        className="bg-center bg-cover"
+        id="main"
+        className="bg-center bg-cover bg-fixed min-h-full"
         style={{
-          backgroundImage: `url(${recipe?.image})`,
+          backgroundImage: `url(${image})`,
+        }}
+        onLoad={() => {
+          const tableTopHeight = document.getElementById("table").offsetTop;
+          const tableHeight = document.getElementById("table").offsetHeight;
+          const mainHeight = document.getElementById("main").offsetHeight;
+          const deltaHeight = tableHeight + tableTopHeight - mainHeight;
+          if(deltaHeight > 0)
+          document.getElementById("main").style.height = `${mainHeight + deltaHeight}px`;
         }}
       >
-        <div className=" backdrop-blur-[6px] ">
-          <div className="p-[2%] flex flex-row justify-center items-center">
-            <div className="p-[2%]">
-            <h1 className="font-bold text-center text-xl lg:text-3xl">
-              {recipe?.name}
-            </h1>
-            <p className="text-center text-lg">by {recipe?.user?.username}</p>
-          </div>
-          </div>
-          <div className="flex flex-col md:flex-row-reverse mx-1 text-center text-white">
-            <div className="mb-[5%] md:ml-[4%] md:mb-[2%] grid grid-rows-[235px] grid-cols-2 md:grid-cols-1 md:grid-rows-[auto_300px] w-full md:w-[50%] p-1 font-thin">
-              <div className="p-3 mr-[3%] rounded-[18px] bg-black bg-opacity-25">
-                <img src={Image1} className="rounded-[16px]" />
-                <p className="text-base md:text-lg">My {recipe?.name}</p>
-              </div>
-              <div
-                id="ingredients"
-                className="p-4 overflow-y-auto rounded-[18px] ml-[3%] md:mt-[4%] bg-black bg-opacity-25 "
-              >
-                <p className="font-medium p-1 text-lg border-b-[1px] border-black border-opacity-45">
-                  Ingredients
-                </p>
-                <div className="text-white rounded-[18px] bg-black bg-opacity-50">
-                  {recipe?.ingredients.map((ingredient) => {
-                    return (
-                      <div
-                        key={ingredient?.ingredient}
-                        className="p-3 flex flex-row text-xs md:text-sm border-b-[1px] border-black border-opacity-50"
-                      >
-                        <div className="font-[400] mr-1 text-end w-[40%]">
-                          {ingredient?.ingredient}:
-                        </div>
-                      </div>
-                    );
-                  })}
-                </div>
+        <div className="min-h-full h-screen backdrop-blur-[6px]">
+          <div className="p-[2%]">
+            <div className="p-3 text-white w-fit flex flex-row rounded-[18px] bg-black bg-opacity-40">
+              <img
+                src={image}
+                alt="recipe image"
+                className="rounded-[16px] h-[200px]"
+              />
+              <div className="ml-3 flex flex-col justify-end">
+                <h1 className="font-bold p-2 text-xl lg:text-[44px]">
+                  {recipe?.name}
+                </h1>
+                <span className="text-lg p-2">by {recipe?.user?.username}</span>
               </div>
             </div>
-            <div
-              id="instructions"
-              className="flex flex-col p-4 mb-[2%] ml-1 md:mx-[2%] h-[600px] md:h-[700px] bg-black bg-opacity-25 rounded-[18px]"
-            >
-              <p className="font-medium mb-2 text-lg">Instructions</p>
-              <div className="p-3 font-thin rounded-[16px] text-white bg-black bg-opacity-50 mx-2">
-                {recipe?.instructions.map((instruction) => {
-                  return (
-                    <div
-                      key={instruction.instruction}
-                      className="p-2 text-sm md:text-lg text-start"
-                    >
-                      <span className="font-medium">
-                        {recipe.instructions.indexOf(instruction) + 1}
-                      </span>
-                      : {instruction.instruction}
-                    </div>
-                  );
-                })}
+          </div>
+          <div id="table" className="p-[2%]">
+            <div className="flex flex-col  md:flex-row mx-1 text-center text-white rounded-lg bg-black bg-opacity-30">
+              <div
+                id="ingredients"
+                className="p-4 md:w-[45%] overflow-y-auto md:border-r-[1px] border-white border-opacity-35"
+              >
+                <h2 className="font-medium mb-2 text-2xl md:border-b-[1px] border-white border-opacity-35">
+                  Ingredients
+                </h2>
+                <div className="p-3  min-h-[70px] text-white rounded-[16px] ">
+                  {recipe?.ingredients.map(drawIngredients)}
+                </div>
+              </div>
+              <div
+                id="instructions"
+                className="flex flex-col p-4 md:w-[55%] rounded-[18px]"
+              >
+                <h2 className="font-medium mb-2 text-2xl border-b-[1px] border-white border-opacity-35">
+                  Instructions
+                </h2>
+                <div className="p-3 min-h-[70px] font-thin rounded-[16px] text-white">
+                  {recipe?.instructions.map(drawInstructions)}
+                </div>
               </div>
             </div>
           </div>
