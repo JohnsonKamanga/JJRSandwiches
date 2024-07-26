528bb43e18c5a8cb34fad8f019b6bd9b5d2b3138
JohnsonKamanga <johnsonkamanga2@gmail.com>
2024-07-26 23:05:47 +0200

    removed the orderby options, might be implemented in the future

--- FrontEnd/src/Components/Search/SearchBar.jsx
+++ FrontEnd/src/Components/Search/SearchBar.jsx
@@ -15,6 +15,68 @@ export default function SearchBar(props) {
   const loadingState = props.loadingState;
   const setLoadingState = props.setLoadingState;
   const [searchQuery, setSearchQuery] = useState("");
+  const [showPopUp, setShowPopUp] = useState(false);
+  const [currentOrderByOption, setCurrentOrderByOption] = useState(0);
+  const OrderByOptions = ["OrderBy", "Name", "Ingredients", "Time"];
+
+  const dropDown = <div
+  className="p-1 bg-white flex flex-row w-[16%] lg:w-[8%] justify-center items-center rounded-xl border-black border-opacity-25 transition-all duration-200 hover:border-opacity-65 border-[1px] hover:cursor-pointer focus:border-opacity-65 relative"
+  onClick={async () => {
+    const drop = document.getElementById("popup");
+    if (showPopUp === false) {
+      drop.style.display = "block";
+      await wait(200);
+      drop.style.opacity = "1.0";
+    } else {
+      drop.style.opacity = "0.0";
+      await wait(200);
+      drop.style.display = "none";
+      
+    }
+    setShowPopUp(!showPopUp);
+  }}
+>
+  <span className="font-medium text-xs md:text-sm p-1">
+    {OrderByOptions[currentOrderByOption]}
+  </span>
+  {showPopUp ? (
+    <FontAwesomeIcon
+      icon={faCaretDown}
+      className="w-[8px] md:w-[12px] h-[8px] md:h-[12px] ml-[0.5%]"
+    />
+  ) : (
+    <FontAwesomeIcon
+      icon={faAngleDown}
+      className="w-[8px] md:w-[12px] h-[8px] md:h-[12px] ml-[0.5%]"
+    />
+  )}
+    <div id="popup" className="hidden transition-opacity duration-[150ms] opacity-0 w-[120%] p-2 text-sm rounded-md border-[1px] border-black bg-white border-opacity-60 absolute top-[110%]  left-[50%] z-40">
+      <div
+        className=" border-b-[1px] border-black border-opacity-20 hover:font-semibold hover:cursor-pointer"
+        onClick={() => setCurrentOrderByOption(0)}
+      >
+        Default
+      </div>
+      <div
+        className=" border-b-[1px] border-black border-opacity-20 hover:font-semibold hover:cursor-pointer"
+        onClick={() => setCurrentOrderByOption(1)}
+      >
+        Name
+      </div>
+      <div
+        className=" border-b-[1px] border-black border-opacity-20 hover:font-semibold hover:cursor-pointer"
+        onClick={() => setCurrentOrderByOption(2)}
+      >
+        Ingredients
+      </div>
+      <div
+        className="hover:font-semibold hover:cursor-pointer"
+        onClick={() => setCurrentOrderByOption(3)}
+      >
+        Time
+      </div>
+    </div>
+</div>
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -72,15 +134,9 @@ export default function SearchBar(props) {
     } 
   };
 
-  const [showPopUp, setShowPopUp] = useState(false);
-
-  const [currentOrderByOption, setCurrentOrderByOption] = useState(0);
-
-  const OrderByOptions = ["OrderBy", "Name", "Ingredients", "Time"];
-
   return (
     <div className="min-w-full h-[15%] md:h-[7%] lg:h-[8%] xl:h-[15%] flex flex-col items-center p-2">
-      <div className="w-[60%] flex flex-row items-center mb-2 border-[1px] rounded-xl border-black transition-all duration-200 border-opacity-20 hover:border-opacity-65 focus-within:border-opacity-65">
+      <div className="w-[75%] sm:w-[60%] flex flex-row items-center mb-2 border-[1px] rounded-xl border-black transition-all duration-200 border-opacity-20 hover:border-opacity-65 focus-within:border-opacity-65">
         <form
           className="flex flex-row items-center bg-white rounded-xl w-[100%]"
           onSubmit={handleSubmit}
@@ -88,7 +144,7 @@ export default function SearchBar(props) {
           <input
             type="text"
             name="search"
-            className="rounded-l-xl mx-2 p-1 text-sm w-[85%] sm:w-[90%] md:w-[95%] border-0 border-opacity-0 border-none outline-none "
+            className="rounded-l-xl mx-2 p-1 text-xs sm:text-sm w-[85%] sm:w-[90%] md:w-[95%] border-0 border-opacity-0 border-none outline-none "
             placeholder="search"
             onChange={(e) => {
               e.preventDefault();
@@ -97,7 +153,7 @@ export default function SearchBar(props) {
             value={searchQuery}
           ></input>
           <button
-          className="w-[8%]"
+          className=" w-[8%]"
           type="submit">
             {" "}
             <FontAwesomeIcon
@@ -107,64 +163,7 @@ export default function SearchBar(props) {
           </button>
         </form>
       </div>
-      <div
-        className="p-1 bg-white flex flex-row w-[16%] lg:w-[8%] justify-center items-center rounded-xl border-black border-opacity-25 transition-all duration-200 hover:border-opacity-65 border-[1px] hover:cursor-pointer focus:border-opacity-65 relative"
-        onClick={async () => {
-          const drop = document.getElementById("popup");
-          if (showPopUp === false) {
-            drop.style.display = "block";
-            await wait(200);
-            drop.style.opacity = "1.0";
-          } else {
-            drop.style.opacity = "0.0";
-            await wait(200);
-            drop.style.display = "none";
-            
-          }
-          setShowPopUp(!showPopUp);
-        }}
-      >
-        <span className="font-medium text-xs md:text-sm p-1">
-          {OrderByOptions[currentOrderByOption]}
-        </span>
-        {showPopUp ? (
-          <FontAwesomeIcon
-            icon={faCaretDown}
-            className="w-[8px] md:w-[12px] h-[8px] md:h-[12px] ml-[0.5%]"
-          />
-        ) : (
-          <FontAwesomeIcon
-            icon={faAngleDown}
-            className="w-[8px] md:w-[12px] h-[8px] md:h-[12px] ml-[0.5%]"
-          />
-        )}
-          <div id="popup" className="hidden transition-opacity duration-[150ms] opacity-0 w-[120%] p-2 text-sm rounded-md border-[1px] border-black bg-white border-opacity-60 absolute top-[110%]  left-[50%] z-40">
-            <div
-              className=" border-b-[1px] border-black border-opacity-20 hover:font-semibold hover:cursor-pointer"
-              onClick={() => setCurrentOrderByOption(0)}
-            >
-              Default
-            </div>
-            <div
-              className=" border-b-[1px] border-black border-opacity-20 hover:font-semibold hover:cursor-pointer"
-              onClick={() => setCurrentOrderByOption(1)}
-            >
-              Name
-            </div>
-            <div
-              className=" border-b-[1px] border-black border-opacity-20 hover:font-semibold hover:cursor-pointer"
-              onClick={() => setCurrentOrderByOption(2)}
-            >
-              Ingredients
-            </div>
-            <div
-              className="hover:font-semibold hover:cursor-pointer"
-              onClick={() => setCurrentOrderByOption(3)}
-            >
-              Time
-            </div>
-          </div>
-      </div>
+      
     </div>
   );
 }
