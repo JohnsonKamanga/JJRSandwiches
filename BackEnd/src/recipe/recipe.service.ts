dc0028d22f9d612a8899e44ce46dd1a1d0209a95
JohnsonKamanga <johnsonkamanga2@gmail.com>
2024-07-31 20:53:41 +0200

    added a function to find recipes by user and improved the update function logic

--- BackEnd/src/recipe/recipe.service.ts
+++ BackEnd/src/recipe/recipe.service.ts
@@ -69,6 +69,24 @@ export class RecipeService {
     });
   }
 
+  async findByUser(user): Promise<Recipe[]> {
+    return await this.recipeRepository.find({
+      select: {
+        id: true,
+        name: true
+      },
+
+      where: {
+        user: user,
+      },
+      relations: {
+        user: true,
+        ingredients: true,
+        instructions: true,
+      },
+    });
+  }
+
   async create(body): Promise< Recipe | null>{
     let {instructions, ingredients, ...result} = body;
     const recipe = await this.recipeRepository.save(result);
@@ -87,7 +105,88 @@ export class RecipeService {
   }
 
   async update(id, body): Promise<any>{
-    return await this.recipeRepository.update(id,body)
+    const {instructions, ingredients, ...recipe} = body;
+    const currentIngredients = await this.ingredientsService.findOneByRecipe({id});
+    const currentInstructions = await this.instructionsService.findByRecipe({id});
+
+    if(ingredients){
+      //will store ingredients to be deleted
+      let toBeDeleted = [];
+
+      if(ingredients.length > 0){
+      //check if ingredient is still available, if not then delete
+    for(let i = 0 ; i < currentIngredients.length ; i++){
+      for(let j = 0 ; j < ingredients.length ; j++ ){
+        //if no id is present then contiunue to next iteration
+        if(!ingredients[j]?.id){
+          continue;
+        }
+        //if currentIngredient id is present in updated ingredients then break
+        else if(currentIngredients[i].id === ingredients[j]?.id){
+            break;
+        }
+        /*if currentIngredient id is not present at the end of the loop,
+        add to list of ingredients to be deleted
+        */  
+        else if(j === ingredients.length - 1){
+          toBeDeleted.push(currentIngredients[i]);
+        }
+      }
+    }
+  }
+  else{
+    toBeDeleted = currentIngredients;
+  }
+    //delete unavailable ingredients
+    if(toBeDeleted.length > 0){
+      toBeDeleted.forEach((ingredient)=>{
+        this.ingredientsService.delete(ingredient.id);
+      })
+    }
+
+    await this.ingredientsService.bulkUpdate(ingredients, recipe);
+    }
+
+    if(instructions){
+       //will store instructions to be deleted
+       let toBeDeleted = [];
+
+       if(instructions.length > 0){
+       //check if instruction is still available, if not then delete
+     for(let i = 0 ; i < currentInstructions.length ; i++){
+       for(let j = 0 ; j < instructions.length ; j++ ){
+         //if no id is present then contiunue to next iteration
+         if(!instructions[j]?.id){
+           continue;
+         }
+         //if currentInstruction id is present in updated instructions then break
+         else if(currentInstructions[i].id === instructions[j]?.id){
+             break;
+         }
+         /*if currentInstruction id is not present at the end of the loop,
+         add to list of instructions to be deleted
+         */  
+         else if(j === instructions.length - 1){
+           toBeDeleted.push(currentInstructions[i]);
+         }
+       }
+     }
+    }
+    else{
+      toBeDeleted = currentInstructions;
+    }
+     //delete unavailable instructions
+     if(toBeDeleted.length > 0){
+       toBeDeleted.forEach((instruction)=>{
+         this.instructionsService.delete(instruction.id);
+       })
+     }
+
+    await this.instructionsService.bulkUpdate(instructions, recipe);
+    
+  }
+    
+    return await this.recipeRepository.update(id,recipe);
   }
 
   async delete(id): Promise<any>{
