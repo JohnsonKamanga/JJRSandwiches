e5ae2159c771752765ab3f1026707bdb5d81d079
JohnsonKamanga <johnsonkamanga2@gmail.com>
2024-07-31 16:04:52 +0200

    improved styling and added a gray background color

--- FrontEnd/src/Components/Communities/Communities.jsx
+++ FrontEnd/src/Components/Communities/Communities.jsx
@@ -1,6 +1,6 @@
 import NavBar from "../HomePage/NavBar";
 import Footer from "../HomePage/Footer";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSpinner, faUserCircle } from "@fortawesome/free-solid-svg-icons";
 import SearchBar from "../Search/SearchBar";
@@ -8,10 +8,12 @@ import { NavLink } from "react-router-dom";
 import axios from "axios";
 import NewCommunity from "./NewCommunitt";
 import { baseurl } from "../../routes";
+import { UserContext } from "../Accounts/UserContext";
 
 export default function Communities() {
   const [communities, setCommunities] = useState([]);
   const [loading, setLoading] = useState(true);
+  const {token} = useContext(UserContext);
   const [errorMessage, setErrorMessage] = useState('');
   const [error, setError] = useState(false);
   const getImage = (community)=>{
@@ -24,55 +26,52 @@ export default function Communities() {
     .catch((err)=>console.error(err));
   }
 
-  useEffect(() => {
-    const unsubscribe = () => {
-      axios
-        .get(`${baseurl}/communities`)
-        .then((fetchedCommunities) => {
-          setCommunities(fetchedCommunities.data);
-          setLoading(false);
-        })
-        .catch((err) => {
-          console.error(err);
-          setErrorMessage(`Error: ${err.errorMessage}`);
-          setError(true);
-        });
-    };
+  const unsubscribe = () => {
+    axios
+      .get(`${baseurl}/communities`)
+      .then((fetchedCommunities) => {
+        setCommunities(fetchedCommunities.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error(err);
+        setErrorMessage(`Error: ${err.errorMessage}`);
+        setError(true);
+      });
+  };
 
-    return () => unsubscribe();
+  useEffect(() => { 
+    unsubscribe();
   }, []);
 
   const drawCommunities = (community) => {
     return (
       <div
         key={community.id}
-        className="border-[1px] m-2 text-white border-white border-opacity-25 hover:border-opacity-45 rounded-[32px] my-1 transition-colors duration-[500ms] bg-black bg-opacity-35 hover:bg-opacity-65 p-[3px]"
+        className="border-[1px] w-full sm:w-fit sm:m-2 text-white border-white border-opacity-25 hover:border-opacity-45 rounded-[32px] my-1 transition-colors duration-[500ms] bg-black bg-opacity-35 hover:bg-opacity-65 p-[6px] sm:p-3"
       >
-        <div className="flex flex-col p-1 rounded-[28px] items-center border-[1px] border-white border-opacity-30">
-          <div className="flex flex-row items-center">
-            <div className="">
-              <div className="rounded-full mr-1 h-20 w-20 flex items-center justify-center bg-black bg-opacity-60 p-1 ">
-                <img className="h-fit w-fit rounded-full" id={community.id} onLoadStart={getImage(community)} alt={community.name} />
+        <div className="flex flex-col sm:min-h-fit p-1 rounded-[28px] items-center border-[1px] border-white border-opacity-30">
+          <div className="flex flex-row items-center w-full sm:w-fit">
+            <div className="w-[30%] flex items-center justify-center sm:mr-1 ">
+              <div className="rounded-full h-[40px] w-[40px] sm:h-full sm:w-full flex items-center justify-center bg-black bg-opacity-60 p-1 sm:p-[6px] ">
+                <img className="h-full sm:h-fit sm:w-fit rounded-full" id={community.id} onLoadStart={getImage(community)} />
               </div>
             </div>
-            <div className="">
+            <div className="m-[1px] sm:m-0 w-[70%]">
               <NavLink
                 to={`/communities/CommunityPage/${community.id}`}
-                className="font-medium text-center hover:text-[#f29260] lg:text-xl "
+                className="font-medium flex items-center text-start hover:text-[#f29260] text-xs sm:text-sm lg:text-xl "
               >
                 {community.name}
               </NavLink>
             </div>
           </div>
         </div>
-        <div className="p-1 border-[1px] my-3 h-[124px] overflow-hidden text-ellipsis flex flex-col items-center rounded-[24px] bg-black bg-opacity-60 border-white border-opacity-30">
+        <div className="p-1 hidden sm:flex flex-col items-center border-[1px] mt-3 h-[124px] overflow-hidden text-ellipsis rounded-[24px] bg-black bg-opacity-60 border-white border-opacity-30">
           <div className="p-1 font-medium">Description:</div>
           <div className="p-2 text-center font-[150]">
             {community.description}
           </div>
-        </div>
-        <div className="ml-[5%] p-2 lg:text-xs text-[8px] flex">
-          <span className="hidden lg:block">2000000+ members</span>
         </div>
       </div>
     );
@@ -83,15 +82,15 @@ export default function Communities() {
       <div id="loadingScreen">
         <div className="min-h-full h-screen w-full">
         <NavBar/>
-        <div className=" faq-transition h-full w-full flex flex-col items-center justify-center">
+        <div className="h-full w-full flex flex-col items-center justify-center bg-gray-600 bg-opacity-65">
         {error ?
-        (<div className="flex flex-col backdrop-blur-lg  items-center justify-center text-[#ff0000]">
+        (<div className="flex flex-col backdrop-blur-lg bg-black bg-opacity-35 w-[250px] h-[250px] rounded-md items-center justify-center text-[#ff0000]">
           {errorMessage}
         </div>)
         :
-        (<div className="flex flex-col backdrop-blur-lg  items-center justify-center">
-        <FontAwesomeIcon className="animate-spin text-5xl" icon={faSpinner}/>
-        <div className="text-2xl">
+        (<div className="flex flex-col backdrop-blur-lg text-white bg-black bg-opacity-35 w-[250px] h-[250px] rounded-md items-center justify-center">
+        <FontAwesomeIcon className="animate-spin text-3xl" icon={faSpinner}/>
+        <div className="text-xl">
           Loading...
         </div>
         </div>)}
@@ -107,14 +106,14 @@ export default function Communities() {
     <div>
       <div className="flex flex-col min-h-full h-screen">
         <NavBar />
-        <div className=" ">
+        <div className=" h-full sm:h-fit bg-gray-600 bg-opacity-65">
           <div>
             <SearchBar  context="communities" setResults={setCommunities} loadingState={loading} setLoadingState={setLoading} />
-            <div className="flex flex-row p-2 ">
-              <div className=" mx-2 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+            <div className="flex flex-row justify-center p-2 w-full ">
+              <div className=" sm:mx-2 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
                 {communities.map(drawCommunities)}
               </div>
-              <NewCommunity />
+              {token && <NewCommunity communities={communities} setCommunities={setCommunities} />}
             </div>
           </div>
         </div>
