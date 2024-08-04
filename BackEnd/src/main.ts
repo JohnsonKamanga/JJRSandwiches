b09bef5e61063514ec30b568b001dba361873630
JohnsonKamanga <johnsonkamanga2@gmail.com>
2024-08-04 23:50:29 +0200

    added the frontend url as the origin

--- BackEnd/src/main.ts
+++ BackEnd/src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix("api");//all endpoints are accessiblbe through api eg. api/recipe
-  app.enableCors();
+  app.enableCors({
+    origin: 'https://jjr-sandwiches.vercel.app'
+  });
   await app.listen(8000);
 }
 bootstrap();
