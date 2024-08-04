27c5071ddae04ea5d35eaeafd71e10f9b5909fca
JohnsonKamanga <167559755+JohnsonKamanga@users.noreply.github.com>
2024-08-04 15:39:14 +0200

    Delete FrontEnd directory

--- FrontEnd/src/utilities.js
+++
@@ -1,8 +0,0 @@
-export const wait = (ms) => {
-    return new Promise((resolve, reject) => {
-      setTimeout(resolve, ms);
-    });
-  };
-
-export const popUpTrabsitionDuration = 200;
-export const questionTabTransitionDuration = 100;
