777ea8beda39d60538685a6066d712a49bdf45aa
JohnsonKamanga <johnsonkamanga2@gmail.com>
2024-08-05 00:19:54 +0200

    cors set to default options

--- BackEnd/src/main.ts
+++ BackEnd/src/main.ts
@@ -4,9 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix("api");//all endpoints are accessiblbe through api eg. api/recipe
-  app.enableCors({
-    origin: 'https://jjr-sandwiches.vercel.app'
-  });
+  app.enableCors();
   await app.listen(8000);
 }
 bootstrap();
